ba665a9f7eb0f37ce4f73a8324dfb5bff2cafdac
ShahanOne <shahanhussain9105@gmail.com>
2022-02-16 02:10:34 +0530

    initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function Footer() {
     return <div className="footer">
+
    <center><h2> Coded by <a href="https://shahanone.github.io/Shahan_One/">Syed Shahan Hussain</a></h2></center>    
-    </div>
+   
+   </div>
 }
 
 export default Footer;
