74e0e5bb490524c08d802b315e492c3da08e1127
ShahanOne <90277877+ShahanOne@users.noreply.github.com>
2022-05-10 21:56:46 +0530

    Added no-cors heroku server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ function action(ip) {
   
   setIp(ip)
   setClick(true); 
-  fetch("https://api.freegeoip.app/json/"+ip+ "?apikey=6f754640-8d68-11ec-90bb-375f23ee0386")
+  fetch("https://morning-taiga-67409.herokuapp.com/api.freegeoip.app/json/"+ip+ "?apikey=6f754640-8d68-11ec-90bb-375f23ee0386")
   .then(response => response.json())
   .then(data=> setData(data))
   .catch(err => console.log(err))
