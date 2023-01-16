3689c0dde85eb1edfc85fafa5153cb5f86b38742
ShahanOne <shahanhussain9105@gmail.com>
2023-01-16 12:29:44 +0530

    2023

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
-    return <div className="footer">
-
-   <center><h2> Coded by <a href="https://shahanone.github.io/Shahan_One/">Syed Shahan Hussain</a></h2></center>    
-   
-   </div>
+  return (
+    <div className="footer">
+      <center>
+        <h3>
+          {' '}
+          Coded by{' '}
+          <a href="https://shahanone.github.io/Shahan_One/">
+            Syed Shahan Hussain
+          </a>
+        </h3>
+        <h4>
+          <a href=""> 2023</a>
+        </h4>
+      </center>
+    </div>
+  );
 }
 
 export default Footer;
-
